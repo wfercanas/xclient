9f85ef2f09a4f42cc8ca4916d90b8306f38500c8
Fernando Cañas <fernando.canas@outlook.com>
2026-06-08 14:14:33 -0500

    contributions: get contribution by id from database

--- internal/model/contribution.go
+++ internal/model/contribution.go
@@ -13,9 +13,16 @@ type ContributionModel struct {
 }
 
 func (m *ContributionModel) GetContributionById(id string) (Contribution, error) {
-	return Contribution{
-		ProductNumber:      "201-1098717173",
-		ProductDescription: "AHORRO PERMANENTE",
-		BalanceSavings:     323626,
-	}, nil
+	result := m.DB.QueryRow(`
+		SELECT product_number, product_description, balance_savings 
+		FROM fondecom.saving_product_catalog 
+		WHERE product_number = $1`, id)
+
+	var contribution Contribution
+	err := result.Scan(&contribution.ProductNumber, &contribution.ProductDescription, &contribution.BalanceSavings)
+	if err != nil {
+		return Contribution{}, err
+	}
+
+	return contribution, nil
 }
