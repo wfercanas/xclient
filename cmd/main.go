d5c9bb2e9c48e044a96ab42a22f76dc0ef18904a
Fernando Cañas <fernando.canas@outlook.com>
2026-06-07 22:58:01 -0500

    db: open dabatase and create connection pool

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -13,8 +17,16 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Address")
+	dsn := flag.String("dsn", "user=iclient password=iclient host=localhost port=5432 dbname=iclient sslmode=disable", "Data Source Name")
+	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app := &application{
 		Logger: logger,
@@ -25,9 +37,24 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health", health)
 
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, mux)
 	if err != nil {
 		app.Logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		defer db.Close()
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/selsa-inube/iclient-query-service
 
 go 1.23.3
+
+require github.com/lib/pq v1.12.3
